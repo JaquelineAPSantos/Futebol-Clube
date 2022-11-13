e317214358eff875477409e8bd30c75c1a5aee95
Jaqueline Santos <jaque.aps@hotmail.com>
2022-11-13 18:06:17 -0300

    feat: Adicionado o express-async-errors ao app

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import loginRoute from './routes/loginRouter';
+import errorMiddleware from './middlewares/errorMiddleware';
 
 class App {
   public app: express.Express;
@@ -24,6 +25,7 @@ class App {
     this.app.use(express.json());
     this.app.use(accessControl);
     this.app.use('/login', loginRoute);
+    this.app.use(errorMiddleware);
   }
 
   public start(PORT: string | number):void {
