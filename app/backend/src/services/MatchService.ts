8e529d72cc395e3724211edadb61541181e775c8
Jaqueline Santos <jaque.aps@hotmail.com>
2022-11-14 16:51:41 -0300

    feat: Adicionando a função create ao MatchService

--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -10,4 +10,17 @@ export default class MatchesService {
       ],
     });
   }
+
+  static async create(body: {
+    homeTeam: number, awayTeam: number, homeTeamGoals: number, awayTeamGoals: number }) {
+    const match = {
+      homeTeam: body.homeTeam,
+      homeTeamGoals: body.homeTeamGoals,
+      awayTeam: body.awayTeam,
+      awayTeamGoals: body.awayTeamGoals,
+      inProgress: true,
+    };
+
+    return Match.create(match);
+  }
 }
