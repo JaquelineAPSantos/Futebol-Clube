9ca22c108932551a05a1501c4c172b66db2b5bb4
Jaqueline Santos <jaque.aps@hotmail.com>
2022-11-14 12:49:47 -0300

    test: Adicionando teste de login sem email válido

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -15,6 +15,7 @@ const { expect } = chai;
 const adminValid = { email: 'admin@admin.com', password: 'secret_admin' };
 const adminNoPwd = { email: 'admin@admin.com', password: '' };
 const adminBadPwd = { email: 'admin@admin.com', password: 'secret' };
+const adminNoEmail = { email: '', password: 'secret' };
 
 const user = {
   "id": 1,
@@ -45,10 +46,18 @@ describe('Endpoint /login', () => {
       expect(chaiHttpResponse.status).to.equal(200);
   });
 
-  it('Quando a requisição retorna status 400', async () => {
+  it('Quando a requisição retorna status 400 sem password válida', async () => {
     chaiHttpResponse = await chai.request(app)
      .post('/login')
      .send(adminNoPwd)
+    
+     expect(chaiHttpResponse.status).to.equal(400);
+  });
+
+  it('Quando a requisição retorna status 400 sem email válido', async () => {
+    chaiHttpResponse = await chai.request(app)
+     .post('/login')
+     .send(adminNoEmail)
     
      expect(chaiHttpResponse.status).to.equal(400);
   });
