69a3cf4d756e3386d5f8a4cc3de6ea85fa5209cf
Jaqueline Santos <jaque.aps@hotmail.com>
2022-11-14 11:17:43 -0300

    feat: Adicionando a rota de validate e o express-async-errors na importação

--- app/backend/src/routes/loginRouter.ts
+++ app/backend/src/routes/loginRouter.ts
@@ -1,9 +1,13 @@
 import * as express from 'express';
+import 'express-async-errors';
 import { Request, Response } from 'express';
 import UserController from '../controllers/UserController';
 
 const loginRoute = express.Router();
 
+loginRoute.get('/users', (req: Request, res: Response) => UserController.getAll(req, res));
 loginRoute.post('/', (req: Request, res: Response) => UserController.login(req, res));
+loginRoute.get('/validate', (req: Request, res: Response) =>
+  UserController.validateLogin(req, res));
 
 export default loginRoute;
