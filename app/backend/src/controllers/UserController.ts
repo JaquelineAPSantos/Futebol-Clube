e8584df4dd862f263948c3170472acf278cb92b8
Jaqueline Santos <jaque.aps@hotmail.com>
2022-11-14 11:22:46 -0300

    feat: Adicionando a função validateLogin e getAll e refatorando a função login implementando a criação do token para o user

--- app/backend/src/controllers/UserController.ts
+++ app/backend/src/controllers/UserController.ts
@@ -2,15 +2,33 @@ import bcrypt = require('bcryptjs');
 import { Request, Response } from 'express';
 import UserService from '../services/UserService';
 import validateLogin from '../middlewares/validateLogin';
+import { createToken, validateToken } from '../services/jwtService';
 
 export default class UserController {
+  static async getAll(_req: Request, res: Response) {
+    const user = await UserService.getUsers();
+    return res.status(200).json(user);
+  }
+
   static async login(req: Request, res: Response) {
     const { email, password } = req.body;
     validateLogin({ email, password });
     const user = await UserService.findOne(email);
-    if (!user) res.status(400).json({ message: 'UnauthorizedError' });
-    if (user) bcrypt.compareSync(password, user.password);
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return res.status(401).json({ message: 'Incorrect email or password' });
+    }
+    const token = createToken(email, password);
+    return res.status(200).json({ token });
+  }
 
-    return res.status(200).json(user);
+  static async validateLogin(req: Request, res: Response) {
+    const { authorization } = req.headers;
+    if (authorization) {
+      const validated = validateToken(authorization);
+      const user = await UserService.findOne(validated.email);
+      if (user) {
+        return res.status(200).json({ role: user.role });
+      }
+    }
   }
 }
