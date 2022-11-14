6e5e5d5d6b3c1b40d9ab893d0e7de6282834ade5
Jaqueline Santos <jaque.aps@hotmail.com>
2022-11-14 16:50:47 -0300

    feat: Adicionando a função saveInProgress

--- app/backend/src/controllers/MatchController.ts
+++ app/backend/src/controllers/MatchController.ts
@@ -1,9 +1,22 @@
 import { Request, Response } from 'express';
-import MatcheService from '../services/MatchService';
+import MatchService from '../services/MatchService';
+import { validateToken } from '../services/jwtService';
 
 export default class MatchController {
   static async getAll(_req: Request, res: Response) {
-    const teams = await MatcheService.getTeams();
+    const teams = await MatchService.getTeams();
     return res.status(200).json(teams);
+  }
+
+  static async saveInProgress(req: Request, res: Response) {
+    const { authorization } = req.headers;
+    if (authorization) {
+      validateToken(authorization);
+      const team = await MatchService.create(req.body);
+
+      if (team) {
+        return res.status(201).json(team);
+      }
+    }
   }
 }
